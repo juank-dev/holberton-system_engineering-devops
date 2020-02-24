e795fbf711e351213a551f838a1d0f397f0140bb
Juan-Bogota <1213@holbertonschool.com>
2020-02-24 21:16:42 +0000

    [upload] Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-print ARGV[0].scan(/from:(\w+)/).join
-print ","
-print ARGV[0].scan(/to:(\+?\d+)/).join
-print ","
-puts ARGV[0].scan(/flags:(\-?\d:\-?\d:\-?\d:\-?\d:\-?\d)/).join
+print ARGV[0].scan(/from:(\w+)/).join + "," + ARGV[0].scan(/to:(\+?\d+)/).join + "," + ARGV[0].scan(/flags:(\-?\d:\-?\d:\-?\d:\-?\d:\-?\d)/).join + "\n"
