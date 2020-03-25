15ccf35c3ac5d69bfdd615c732de7663352a806f
Juan-Bogota <1213@holbertonschool.com>
2020-03-24 21:46:22 -0500

    [Feature] SSH Config

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet conf SSH client configuration
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton'
+}
