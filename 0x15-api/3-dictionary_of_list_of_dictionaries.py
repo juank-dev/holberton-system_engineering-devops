8a1918139f2fba3ef5c195b53e3eb8de99916e88
Juan-Bogota <1213@holbertonschool.com>
2020-05-04 20:35:42 -0500

    JSON information ALL employee

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,18 +13,23 @@ if __name__ == "__main__":
     s = requests.get('https://jsonplaceholder.typicode.com/users')
     info_user = s.json()
     info_todos = r.json()
-    l = 0
 
     with open('todo_all_employees.json', mode='w') as json_file:
         info_json = {}
+        list_all = {}
         for x in info_user:
-            user_name = (x.get("username"))
-        for y in info_todos:
-            if l == 0:
-                user = y.get("userId")
-                info_json[user] = []
-            info_json[user].append({"username": user_name,
-                                    "task": y.get("title"),
-                                    "completed": y.get("completed")})
-            l += 1
-        json.dump(info_json, json_file)
+            user_name = x.get("username")
+            my_id = x.get("id")
+            l = 0
+            for y in info_todos:
+                if my_id == y.get("userId"):
+                    if l == 0:
+                        user = y.get("userId")
+                        info_json[user] = []
+                        l = 1
+                    info_json[user].append({"username": user_name,
+                                            "task": y.get("title"),
+                                            "completed": y.get("completed")})
+            print(info_json)
+            list_all = {**list_all, **info_json}
+        json.dump(list_all, json_file)
