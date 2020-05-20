fb6444384a55ec4e29f71bea7c4c33485bb2eca0
Juan Carlos Lopez <55102476+Juan-Bogota@users.noreply.github.com>
2020-05-20 11:30:06 -0500

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Strace is your friend and fix the error
 
 exec { 'Strace':
-  command  => 'sed -i "s/class-wp-locale.pphp/class-wp-locale.php/" /var/www/html/wp-settings.php',
+  command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php',
   provider => 'shell',
 }
